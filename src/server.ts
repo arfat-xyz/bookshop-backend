19e67e510351bc663c7240f2146d67372f143100
arfat-xyz <arfatrahman08@gmail.com>
2023-07-27 14:57:53 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import { config } from "./config";
+const port = 3000;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    console.log("Db is connected");
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(`Failed to connect database ${error}`);
+  }
+}
+bootstrap();
